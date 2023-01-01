a96f6c1e5e9fffb0d4bdd96da29667daeeab3b59
Gabriel Barahona <gbaralb1@gmail.com>
2022-12-31 23:45:24 -0800

    Added bullet and enemy scripts

--- mustymania_game/Assets/Bullet.cs
+++ mustymania_game/Assets/Bullet.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null) {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+
     }
 
     // Update is called once per frame
