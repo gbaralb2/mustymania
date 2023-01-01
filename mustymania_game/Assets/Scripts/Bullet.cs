b3a9079b2b59cee3363d46f5a93540029fb62c9c
Gabriel Barahona <gbaralb1@gmail.com>
2023-01-01 15:41:23 -0800

    Impact/Death Effects

--- mustymania_game/Assets/Scripts/Bullet.cs
+++ mustymania_game/Assets/Scripts/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
     public float speed = 20f;
     public int damage = 40;
     public Rigidbody2D rb;
+    public GameObject impactEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +22,10 @@ public class Bullet : MonoBehaviour
         if (enemy != null) {
             enemy.TakeDamage(damage);
         }
-        Destroy(gameObject);
 
+        Instantiate(impactEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
