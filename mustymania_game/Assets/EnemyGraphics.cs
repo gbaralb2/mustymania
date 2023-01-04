8598c8fcdaa41d346cf72c0571617c1f5d3e3ad2
davidcsarabia <121732489+davidcsarabia@users.noreply.github.com>
2023-01-04 13:10:51 -0800

    Size Adjustment

--- mustymania_game/Assets/EnemyGraphics.cs
+++ mustymania_game/Assets/EnemyGraphics.cs
@@ -12,11 +12,11 @@ public class EnemyGraphics : MonoBehaviour
     {
         if(aiPath.desiredVelocity.x >= 0.01f)
         {
-            transform.localScale = new Vector3(-6f, 8f, 1f);
+            transform.localScale = new Vector3(-5f, 7f, 1f);
         }
         else if (aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(6f, 8f, 1f);
+            transform.localScale = new Vector3(5f, 7f, 1f);
         }
     }
 }
